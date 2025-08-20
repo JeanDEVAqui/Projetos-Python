b617108f977f6ff18139c9905ade0a02199219ae
JeanDEVAqui <jeangabrielpta2015@gmail.com>
2025-08-20 13:56:30 -0300

    Update-Correcao-Resultado
    Correção da exibição do resultado que fechava depois do segundo numero ser inserido.

--- CalculadoraSimples/Calculadora.py
+++ CalculadoraSimples/Calculadora.py
@@ -1,19 +1,29 @@
 def calculadora():
-    num1 = float(input("Digite o primeiro número: "))
-    operador = input("Digite o operador (+, -, *, /): ")
-    num2 = float(input("Digite o segundo número: "))
+    while True:
+        num1 = float(input("Digite o primeiro número: "))
+        operador = input("Digite o operador (+, -, *, /) ou 's' para sair: ")
+        if operador.lower() == 's':
+            print("Saindo da calculadora...")
+            break
+        num2 = float(input("Digite o segundo número: "))
 
-    if operador == "+":
-        resultado = num1 + num2
-    elif operador == "-":
-        resultado = num1 - num2
-    elif operador == "*":
-        resultado = num1 * num2
-    elif operador == "/":
-        resultado= num1 / num2
-    else:
-        print("Operador inválido!")
+        if operador == "+":
+            resultado = num1 + num2
+        elif operador == "-":
+            resultado = num1 - num2
+        elif operador == "*":
+            resultado = num1 * num2
+        elif operador == "/":
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                print("Erro: divisão por zero!")
+                continue
+        else:
+            print("Operador inválido!")
+            continue
 
-    print("O resultado é:", resultado)
+        print("O resultado é:", resultado)
+        print("-" * 30)  # linha separadora
 
 calculadora()
